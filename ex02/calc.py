3cbe9d7b04e77effff5e3007368cc548b61ec208
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-04 14:37:02 +0900

    ボタン作成

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,4 +3,13 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("300x500")
 
+num = 9
+for i in range(4):
+    for j in range(3):
+        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30))
+        if num >= 0:
+            button.grid(column=j,row=i)
+            num -= 1
+
+
 root.mainloop()
