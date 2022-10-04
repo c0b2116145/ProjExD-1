d609117531e2bea0d6ba03854cf4bf4a8a9b72d1
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-04 14:56:01 +0900

    テキストボックス追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,7 +7,11 @@ def button_click(event):
     tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x580")
+
+entry = tk.Entry(root, width=10,font=("Times New Roman",40),justify="right")
+entry.grid(column=0,row=0,columnspan=3)
+
 
 num = 9
 for i in range(4):
@@ -15,7 +19,7 @@ for i in range(4):
         button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30), command=button_click)
         button.bind("<1>",button_click)
         if num >= 0:
-            button.grid(column=j,row=i)
+            button.grid(column=j,row=i+1)
             num -= 1
 
 
