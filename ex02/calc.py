fe71b30299b3807d6f5d7605487df2f59e3f46bc
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-04 15:52:05 +0900

    レイアウト変更

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,28 +20,28 @@ root.title("電卓")
 root.geometry("380x580")
 
 entry = tk.Entry(root, width=10,font=("Times New Roman",40),justify="right")
-entry.grid(column=0,row=0,columnspan=3)
+entry.grid(column=0,row=0,columnspan=10)
 
 
 num = 9
 for i in range(4):
     for j in range(3):
-        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30))
+        button = tk.Button(root,text=f"{num}",height=1,width=4,font=("Times New Roman", 30))
         button.bind("<1>",button_click)
         if num >= 0:
-            button.grid(column=j,row=i+1)
+            button.grid(column=j+1,row=i+2)
             num -= 1
 
-button = tk.Button(root,text="+",height=2,width=4,font=("Times New Roman", 30))
+button = tk.Button(root,text="+",height=1,width=4,font=("Times New Roman", 30))
 button.bind("<1>",button_click)
-button.grid(column=1,row=4)
+button.grid(column=4,row=4)
 
-button = tk.Button(root,text="=",height=2,width=4,font=("Times New Roman", 30))
+button = tk.Button(root,text="=",height=1,width=4,font=("Times New Roman", 30),bg="gray")
 button.bind("<1>",equal)
-button.grid(column=2,row=4)
+button.grid(column=4,row=5)
 
-button = tk.Button(root,text="c",height=2,width=4,font=("Times New Roman", 30))
+button = tk.Button(root,text="c",height=1,width=4,font=("Times New Roman", 30),bg="blue")
 button.bind("<1>",c_click)
-button.grid(column=4,row=1)
+button.grid(column=1,row=1)
 
 root.mainloop()
