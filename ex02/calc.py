2ba4a4d7dd2a5ecaa4e178c387aeb159342b1ae3
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-04 14:40:50 +0900

    ボタン反応

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -6,7 +12,8 @@ root.geometry("300x500")
 num = 9
 for i in range(4):
     for j in range(3):
-        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30))
+        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30), command=button_click)
+        button.bind("<1>",button_click)
         if num >= 0:
             button.grid(column=j,row=i)
             num -= 1
