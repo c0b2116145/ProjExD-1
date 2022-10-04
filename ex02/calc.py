4d91e8e942f0710f1f6f300e6dbda24bcf4db5bd
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-04 15:10:13 +0900

    ボックスに数字挿入

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,8 +3,8 @@ import tkinter.messagebox as tkm
 
 def button_click(event):
     btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
+    num = int(btn["text"])
+    entry.insert(tk.END,num)
 
 root = tk.Tk()
 root.geometry("300x580")
@@ -16,7 +16,7 @@ entry.grid(column=0,row=0,columnspan=3)
 num = 9
 for i in range(4):
     for j in range(3):
-        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30), command=button_click)
+        button = tk.Button(root,text=f"{num}",height=2,width=4,font=("Times New Roman", 30))
         button.bind("<1>",button_click)
         if num >= 0:
             button.grid(column=j,row=i+1)
