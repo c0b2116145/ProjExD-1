a5ca86ff2978ba5c3a26c7f7dcb1ca47e453ac5c
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-18 15:20:05 +0900

    練習11

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,15 +10,18 @@ def key_up(event):
     key = ""
 
 def main_proc():
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    
+    cx, cy = mx*100+50, my*100+50
 
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
@@ -37,7 +40,8 @@ if __name__ == "__main__":
     mm.show_maze(canv, maze_list)
 
     tori = tk.PhotoImage(file="fig/5.png")
-    cx,cy = 300,400
+    mx, my = 1,1
+    cx,cy = mx*100+50, my*100+50
     canv.create_image(cx,cy,image=tori,tag="tori")
 
     key = ""
