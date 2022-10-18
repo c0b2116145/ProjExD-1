72481e9aa595afc1ebda969b55955158c194f1de
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-18 17:09:49 +0900

    コメント追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,18 +1,19 @@
 import tkinter as tk
 import maze_maker as mm
 
-def key_down(event):
+def key_down(event): #キーを押したら
     global key
-    key =event.keysym
+    key =event.keysym #押したキーの取得
 
-def key_up(event):
+def key_up(event): #キーを離したら
     global key
-    key = ""
+    key = "" #リセット
 
-def main_proc():
+def main_proc(): #メインの関数
     global mx, my
     global cx, cy
     global maze_list
+    #上下左右の処理
     if key == "Up":
         my -= 1
     if key == "Down":
@@ -22,6 +23,7 @@ def main_proc():
     if key == "Right":
         mx += 1
 
+    #もし壁だったら上記の処理を無かったことにする
     if maze_list[my][mx] == 1:
         if key == "Up":
             my += 1
@@ -32,36 +34,44 @@ def main_proc():
         if key == "Right":
             mx -= 1
 
+    #もしゴールだったらテキストを"GOAL"にし停止する
     elif maze_list[my][mx] == 3:
-        cx, cy = mx*100+50, my*100+50
+        cx, cy = mx*100+50, my*100+50 #こうかとんの場所の更新
         canv.coords("tori", cx, cy)
-        text.set("GOAL")
-        return
+        text.set("GOAL") #テキストをゴールにする
+        return #<-再起関数を呼び出させないようにする
 
+    #それ以外ならこうかとんの場所を更新する
     else:
         cx, cy = mx*100+50, my*100+50
 
+    #もし"r"を押したら
     if key == "r":
-        canv.delete("tori")
-        cx, cy = 150, 150
-        mx, my = 1,1
+        canv.delete("tori")             #canv内のこうかとんの"tori"のタグを削除する
+        #こうかとんの場所の初期化
+        mx, my = 1, 1
+        cx, cy = mx*100+50,my*100+50
+        #迷路の再構築  
         maze_list = mm.make_maze(15,9)
         mm.show_maze(canv, maze_list)
+        #こうかとんの描画
         canv.create_image(cx,cy,image=tori,tag="tori")
 
         
-
+    #canvの更新
     canv.coords("tori", cx, cy)
+    #再起
     root.after(100,main_proc)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-
+    #テキストの設定
     text = tk.StringVar()
     text.set("Play")
 
     root.title("迷えるこうかとん")
+    #textvariableでテキストの更新を可能にする
     label = tk.Label(root,textvariable=text,font=("",100))
     label.pack()
 
@@ -69,7 +79,6 @@ if __name__ == "__main__":
     canv.pack()
 
     maze_list = mm.make_maze(15,9)
-    # print(maze_list)
 
     mm.show_maze(canv, maze_list)
 
