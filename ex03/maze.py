b2c74b4bb526db6398c18e1ee90cedeff24449bd
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-18 18:12:54 +0900

    修正 #8

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import maze_maker as mm
+from maze_maker import *
 
 def key_down(event): #キーを押したら
     global key
@@ -52,8 +52,8 @@ def main_proc(): #メインの関数
         mx, my = 1, 1
         cx, cy = mx*100+50,my*100+50
         #迷路の再構築  
-        maze_list = mm.make_maze(15,9)
-        mm.show_maze(canv, maze_list)
+        maze_list = make_maze(15,9)
+        show_maze(canv, maze_list)
         #こうかとんの描画
         canv.create_image(cx,cy,image=tori,tag="tori")
 
@@ -78,9 +78,9 @@ if __name__ == "__main__":
     canv = tk.Canvas(root,width=1500,height=900,bg="black")
     canv.pack()
 
-    maze_list = mm.make_maze(15,9)
+    maze_list = make_maze(15,9)
 
-    mm.show_maze(canv, maze_list)
+    show_maze(canv, maze_list)
 
     tori = tk.PhotoImage(file="fig/5.png")
     mx, my = 1,1
