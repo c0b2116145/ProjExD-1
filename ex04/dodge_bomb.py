3da81e78b239246e480c30bcd006ae93dfab1862
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-25 17:04:05 +0900

    コメント追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,7 +2,7 @@ import pygame as pg
 import sys
 import random
 
-def check_bound(obj_rct,scr_rct):
+def check_bound(obj_rct,scr_rct): # 画面外の判別
     """
     obj_rect:こうかとんrct または 爆弾rct
     scr_rct:スクリーンrct
@@ -16,22 +16,28 @@ def check_bound(obj_rct,scr_rct):
 
     return yoko, tate
 
-def check_bomb():
-    check = False
-    if random.randint(0,1000) == 0:
-        check = True
+def check_bomb(): # 爆弾増幅判定（ランダム）
+    check = False # check：Trueなら増幅、Falseならそのまま
+    if random.randint(0,1000) == 0: # 1/1000の確率で爆弾が増える
+        check = True # checkをTrueにする
     return check
 
-def create_bomb(x,y,l):
-    new_bomb_sfc = pg.Surface((20,20))
+def create_bomb(x,y,l): # 新しい爆弾の設定
+    """
+    x:爆弾の位置のx軸
+    y:爆弾の位置のy軸
+    l:爆弾のリスト
+    """
+    new_bomb_sfc = pg.Surface((20,20)) # Surface
     new_bomb_sfc.set_colorkey((0, 0, 0))
     pg.draw.circle(new_bomb_sfc, (255, 0, 0), (10, 10), 10)
-    new_bomb_rct = new_bomb_sfc.get_rect()
+    new_bomb_rct = new_bomb_sfc.get_rect() # Rect
     new_bomb_rct.centerx = x
     new_bomb_rct.centery = y
+    # 爆弾の動きの設定
     move_x = random.randint(-5,5)
     move_y = random.randint(-5,5)
-    l.append([new_bomb_sfc,new_bomb_rct,move_x,move_y])
+    l.append([new_bomb_sfc,new_bomb_rct,move_x,move_y]) # 爆弾のリストに追加
     
 
 
@@ -67,7 +73,7 @@ def main():
     # 練習６
     vx, vy = +5, +5
 
-    bomb = [[bomb_sfc,bomb_rct,vx,vy]]
+    bombs = [[bomb_sfc,bomb_rct,vx,vy]] # 爆弾のリスト作成
 
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct) # 練習２
@@ -96,6 +102,7 @@ def main():
 
         # マウスの場所にこうかとんを移動させる
         yoko, tate = check_bound(tori_rct, scrn_rct)
+        # もし画面外なら中に入るように動く
         if yoko == -1:
             if x < scrn_rct.width/2:
                 x += 1
@@ -106,37 +113,39 @@ def main():
                 y += 1
             if y > scrn_rct.height/2:
                 y -= 1
+        # それ以外はマウスの場所に移動
         else:    
             if event.type == pg.MOUSEMOTION:
                 x, y = event.pos # マウスの座標
             
         tori_rct.center = (x,y) # こうかとんの座標更新
 
-        
-
-
-
-
+        # こうかとんの描画
         scrn_sfc.blit(tori_sfc, tori_rct)
 
-        bom = check_bomb()
-        if bom:
-            now_x, now_y = bomb_rct.center
-            create_bomb(now_x,now_y,bomb)
-            
-        
-        for bombs in bomb:
-            yoko, tate = check_bound(bombs[1], scrn_rct)
+        # 爆弾を増幅させるか判定
+        new_bom = check_bomb()
+        # 増幅できるなら
+        if new_bom:
+            now_x, now_y = bomb_rct.center # 現在の爆弾の場所
+            create_bomb(now_x,now_y,bombs) # 現在の爆弾の場所と爆弾のリスト
+            # 爆弾のリストに新しい爆弾が追加されている
+    
+        for bomb in bombs: # すべての爆弾の動き
+            yoko, tate = check_bound(bomb[1], scrn_rct) # 画面外判定
 
+            # 画面外なら動きの符号を反転させる（反射する）
             if yoko == -1:
-                bombs[2] *= yoko
+                bomb[2] *= yoko
             if tate == -1:
-                bombs[3] *= tate
+                bomb[3] *= tate
 
-            bombs[1].move_ip(bombs[2], bombs[3])
-            scrn_sfc.blit(bombs[0], bombs[1])
+            # 爆弾を動かす
+            bomb[1].move_ip(bomb[2], bomb[3])
+            scrn_sfc.blit(bomb[0], bomb[1])
 
-            if bombs[1].colliderect(tori_rct):
+            #爆弾に当たったら終了
+            if bomb[1].colliderect(tori_rct):
                 return
         
         pg.display.update()
