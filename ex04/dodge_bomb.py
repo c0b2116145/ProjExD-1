51af8ec75ee2b7877a18abc380cddc9275d18abb
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-10-25 15:36:16 +0900

    練習7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,20 @@ import pygame as pg
 import sys
 import random
 
+def check_bound(obj_rct,scr_rct):
+    """
+    obj_rect:こうかとんrct または 爆弾rct
+    scr_rct:スクリーンrct
+    領域内:+1/領域外:-1
+    """
+    yoko,tate = +1,+1
+    if obj_rct.left < scr_rct.left or obj_rct.right > scr_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or obj_rct.bottom > scr_rct.bottom:
+        tate = -1
+
+    return yoko, tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん") # 練習１
     scrn_sfc = pg.display.set_mode((1600,900))
@@ -15,8 +29,8 @@ def main():
     # 練習３
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
-    tori_rect = tori_sfc.get_rect()
-    tori_rect.center = 900, 400
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
 
     # 練習５
     bomb_sfc = pg.Surface((20,20))
@@ -36,12 +50,26 @@ def main():
             if event.type == pg.QUIT: return
         # 練習４
         key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP]: tori_rect.centery -= 1
-        if key_states[pg.K_DOWN]: tori_rect.centery += 1
-        if key_states[pg.K_LEFT]: tori_rect.centerx -= 1
-        if key_states[pg.K_RIGHT]: tori_rect.centerx += 1
+        if key_states[pg.K_UP]: tori_rct.centery -= 1
+        if key_states[pg.K_DOWN]: tori_rct.centery += 1
+        if key_states[pg.K_LEFT]: tori_rct.centerx -= 1
+        if key_states[pg.K_RIGHT]: tori_rct.centerx += 1
 
-        scrn_sfc.blit(tori_sfc, tori_rect)
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+
+        if yoko == -1:
+            if key_states[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+
+        if tate == -1:
+            if key_states[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= 1
+
+        scrn_sfc.blit(tori_sfc, tori_rct)
 
         bomb_rct.move_ip(vx, vy)
         scrn_sfc.blit(bomb_sfc, bomb_rct)
