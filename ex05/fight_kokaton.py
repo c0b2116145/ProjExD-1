2297934200a78c8bc5b23ed5ad27817cf1026275
C0B21005 <c0b21005c4@edu.teu.ac.jp>
2022-11-01 16:54:11 +0900

    こうかとん攻撃

--- ex05/fight_kokaton.py
+++ ex05/fight_kokaton.py
@@ -18,10 +18,10 @@ class Screen:
 
 class Bird:
     key_delta = {
-        pg.K_UP:    [0, -1],
-        pg.K_DOWN:  [0, +1],
-        pg.K_LEFT:  [-1, 0],
-        pg.K_RIGHT: [+1, 0],
+        pg.K_UP:    [0, -3],
+        pg.K_DOWN:  [0, +3],
+        pg.K_LEFT:  [-3 , 0],
+        pg.K_RIGHT: [+3, 0],
     }
 
     def __init__(self, img, zoom, xy):
@@ -75,21 +75,54 @@ class Bomb:
         self.bound += 1
 
         
-
 class Enemy:
-    def __init__(self, img, zoom, xy):
+    def __init__(self, img, zoom, xy, vxy):
         sfc = pg.image.load(img)
         self.sfc = pg.transform.rotozoom(sfc, 0, zoom)
         self.rct = self.sfc.get_rect()
         self.rct.center = xy
+        self.vx, self.vy = vxy
         
 
     def blit(self, scr:Screen):
         scr.sfc.blit(self.sfc, self.rct)
 
     def update(self,scr:Screen):
-        self.rct.centerx += 1
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
         self.blit(scr)
+
+
+class Attack:
+    def __init__(self, color, radius, vxy, scr:Screen, fx, fy):
+        self.sfc = pg.Surface((radius*2, radius*2)) # 空のSurface
+        self.sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+        pg.draw.circle(self.sfc, color, (radius,radius), radius) # 円を描く
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = fx
+        self.rct.centery = fy
+        self.vx, self.vy = vxy
+        self.bound = 0
+        self.move = 0
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self,scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        if yoko == -1 or tate == -1:
+            self.count_bound()
+
+        self.move += 1
+        self.blit(scr)
+    
+    def count_bound(self):
+        self.bound += 1
 
 
 
@@ -109,14 +142,10 @@ def check_bound(obj_rct, scr_rct):
 
 def main():
     scr = Screen("逃げろ！こうかとん", (1600,900), "fig/pg_bg.jpg")
-
-    kkt = Bird("fig/6.png", 2.0, (900, 400))
-
+    kkt = Bird("fig/6.png", 1.0, (900, 400))
     bkd = []
-
-
-    ene = [Enemy("fig/1.png", 1.0, (0,randint(0,900)))]
-
+    ene = [Enemy("fig/1.png", 1.0, (randint(0,900),randint(0,900)), (randint(-2,2),randint(-2,2)))]
+    atk = []
 
     clock = pg.time.Clock() # 練習1
     while True:
@@ -126,26 +155,40 @@ def main():
                 return
 
         kkt.update(scr)
+        for kougeki in atk:
+            if kougeki.move >= 100:
+                atk.remove(kougeki)
 
-        if randint(0,1000) == 0:
-            ene.append(Enemy("fig/1.png", 1.0, (0,randint(0,900))))
-
+            kougeki.update(scr)
+                 
+        if randint(0,100) == 0:
+            ene.append(Enemy("fig/1.png", 1.0, (randint(0,900),randint(0,900)),(randint(-2,2),randint(-2,2))))
 
         for enemy in ene:
             enemy.update(scr)
             if kkt.rct.colliderect(enemy.rct):
                 return
 
-            if randint(0,500) == 0:
-                bkd.append(Bomb((255,0,0), 10, (randint(-2,2),randint(-5,5)), scr, enemy.rct.centerx, enemy.rct.centery))
+            if randint(0,300) == 0:
+                bkd.append(Bomb((255,0,0), 10, (randint(-3,3),randint(-3,3)), scr, enemy.rct.centerx, enemy.rct.centery))
+
+            for attack in atk:
+                if enemy.rct.colliderect(attack.rct):
+                    ene.remove(enemy)
+                    break
 
         for bomb in bkd:
+            bomb.update(scr)
             if bomb.bound == 3:
                 bkd.remove(bomb)
                 break
-            bomb.update(scr)
+
             if kkt.rct.colliderect(bomb.rct): # こうかとんrctが爆弾rctと重なったら
                 return
+
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_SPACE]:
+            atk.append(Attack((0,255,0), 10, (randint(-3,3),randint(-3,3)), scr, kkt.rct.centerx, kkt.rct.centery))
 
         pg.display.update() #練習2
         clock.tick(1000)
